322a4bd130e5a45a5e55a1a5f14664709a57cf8f
mizuho <mizuho128@gmail.com>
2018-12-17 15:58:50 +0900

    measurement_program/measure.m

---
+++ measurement_program/measure.m
@@ -0,0 +1,28 @@
+run init.m
+
+% date
+measurement.date = datetime('now');
+d                = measurement.date;
+
+% parameters and descriptions
+dist                 = input('Enter distance >> ');
+measurement.distance = dist;
+measurement.description = input('Any description? >> ', 's');
+
+% filename
+filename      = sprintf('%d-exp', i);
+tmp_filename  =input(sprintf('Enter filename (default: "%s" >> ', filename), 's');
+
+if isempty(tmp_filename) % default filename
+    i = i+1;
+else
+    filename = tmp_filename;
+end
+
+% wave data
+measurement.speaker = f1500.*mask;
+soundsc(measurement.speaker,Fs);
+sss = recording();
+measurement.microphone = sss;
+
+save_data(sprintf('../data/%d-%d-%d/', year(d), month(d), day(d)), filename, measurement);
