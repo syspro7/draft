046c97c0ae9a38791b49a3f31fff07c9e9771c86
mizuho <mizuho128@gmail.com>
2018-12-17 16:25:26 +0900

    save_data.m return data and check file existance

--- measurement_program/save_data.m
+++ measurement_program/save_data.m
@@ -1,4 +1,4 @@
-function save_data(dir_path, name, data)
+function [d] = save_data(dir_path, name, data)
 % dir_path: directory path to save the data
 % name:     only file name of the data without ext
 
@@ -6,5 +6,14 @@ if ~exist(dir_path, 'dir')
     mkdir(dir_path)
 end
 
-save(sprintf('%s/%s.mat', dir_path, name), 'data');
+full_path = sprintf('%s/%s.mat', dir_path, name);
+
+if exist(full_path, 'file') == 2
+    fprintf('ERROR!! %s already exists\n', full_path);
+else
+    save(full_path, 'data');
+end
+
+d = data;
+
 end
