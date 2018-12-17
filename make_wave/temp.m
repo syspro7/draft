0c2597956bfcdd985df1e86557ebb2370df475f0
mizuho <mizuho128@gmail.com>
2018-12-17 16:02:12 +0900

    make_wave/ update

--- make_wave/temp.m
+++ make_wave/temp.m
@@ -1,11 +1,29 @@
+run init.m
+
+% date
 measurement.date = datetime('now');
-measurement.distance = 6.1;
-%measurement.speaker = mask_rect;
-%measurement.speaker = ra2;
-measurement.speaker = f1500.*mask;
+d                = measurement.date;
+
+% parameters and descriptions
+dist                 = input('Enter distance >> ');
+measurement.distance = dist;
+measurement.description = input('Any description? >> ', 's');
+
+% filename
+filename      = sprintf('%d-exp', i);
+tmp_filename  =input(sprintf('Enter filename (default: "%s" >> ', filename), 's');
+
+if isempty(tmp_filename) % default filename
+    i = i+1;
+else
+    filename = tmp_filename;
+end
+
+% select wave data
+% wav = plot_waves
+measurement.speaker = wav;
 soundsc(measurement.speaker,Fs);
 sss = recording();
 measurement.microphone = sss;
 
-save(['measurement20181120-' num2str(i) '.mat'],'measurement');
-i = i + 1;
+save_data(sprintf('../data/%d-%d-%d/', year(d), month(d), day(d)), filename, measurement);
