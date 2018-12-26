883c6d9eaad752da7a1d306c607ed88644e417cd
nxtgear <rcxgear@gmail.com>
2018-12-26 17:40:37 +0900

    Add estimate_distance_env
    Adjust the start of two sound files by using xcorr.
    Then envelope both sounds and find a point where the difference of the amplitude of the enveloped datas and estimate the difference.

---
+++ measurement_program/estimate_distance_env.m
@@ -0,0 +1,42 @@
+function estimate_distance_env(base,test,testdist,Fs,thre)
+    if ~exist('Fs','var')
+        Fs = 96000;
+    end
+    [base_upperenv, base_lowerenv] = envelope(base,300);
+    [test_upperenv, test_lowerenv] = envelope(test,300);
+    [Rmm, lags] = xcorr(base_upperenv, test_upperenv, 'coeff');
+    [~,b] = max(Rmm); 
+    
+    sa = b-(length(Rmm)+1)/2;
+    figure;
+    grid on; hold on;
+    if sa < 0
+        plot((1:length(base)+sa)*1/Fs,[base(1:end+sa)' test(-sa+1:end)']);
+        plot((1:length(base)+sa)*1/Fs,[base_upperenv(1:end+sa)' test_upperenv(-sa+1:end)']);
+        [maxenv, maxenvind] = max(abs(base_upperenv));
+        plot([1 1]*maxenvind*(1/Fs),[-maxenv maxenv]);
+        plot([1 1]*maxenvind*(1/Fs)+2*testdist/340,[-maxenv maxenv],'--');
+        dist = abs(base_upperenv(1:end+sa)-test_upperenv(-sa+1:end));
+        i = maxenvind;
+        while dist(i) < thre
+            i = i + 1;
+        end
+        ds = dist(i-5:i+5);
+        [~,b] = max(ds);
+        plot([1 1]*(i+b-6)/Fs,[-maxenv maxenv]);
+    else
+        plot((1:length(base)-sa)*1/Fs,[base(sa+1:end)' test(1:end-sa)']);
+        plot((1:length(base)-sa)*1/Fs,[base_upperenv(sa+1:end)' test_upperenv(1:end-sa)']);
+        [maxenv, maxenvind] = max(abs(test_upperenv));
+        plot([1 1]*maxenvind*(1/Fs),[-maxenv maxenv]);
+        plot([1 1]*maxenvind*(1/Fs)+2*testdist/340,[-maxenv maxenv],'--');
+        dist = abs(base_upperenv(sa+1:end)-test_upperenv(1:end-sa));
+        i = maxenvind;
+        while dist(i) < thre
+            i = i + 1;
+        end
+        ds = dist(i-5:i+5);
+        [~,b] = max(ds);
+        plot([1 1]*(i+b-6)/Fs,[-maxenv maxenv]);
+    end
+end
