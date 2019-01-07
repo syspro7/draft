846f7a033df89a5702e6110dd9303e3355e58200
mizuho <mizuho128@gmail.com>
2019-01-07 15:13:15 +0900

    estimate distance helper m file

---
+++ measurement_program/estimate_distance.m
@@ -0,0 +1,22 @@
+
+% select data date
+for i = 1:length(measurements)
+    fprintf('%d: %s\n', i, measurements{i}.date);
+end
+
+env.measure_date = dinput('\nWhich ?', 'env.measure_date');
+
+% which is base data?
+env.measure_data = measurements{env.measure_date}.data;
+for i = 1:length(env.measure_data)
+    fprintf('%d: %s\n', i, env.measure_data{i}.description);
+end
+
+env.measure_base = dinput('\nWhich is base?', 'env.measure_base');
+env.measure_test = dinput('\nWhich is test?', 'env.measure_test');
+env.measure_thre = dinput('\nWhat is thre?', 'env.measure_thre');
+
+d = env.measure_data{env.measure_test};
+estimate_distance_env(env.measure_data{env.measure_base}.microphone, d.microphone, d.distance, d.Fs, env.measure_thre);
+
+clear d
